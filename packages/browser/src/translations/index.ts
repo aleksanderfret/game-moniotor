f667e97b2541b0d01cc67430d832179123005fa0
Aleksander Fret <olekfret80@gmail.com>
2021-09-17 23:15:28 +0200

    [GM-1] Add translations files

---
+++ packages/browser/src/translations/index.ts
@@ -0,0 +1,17 @@
+import en from './en.json';
+import pl from './pl.json';
+
+export const polyfillIntl = async (): Promise<void> => {
+  if (!Intl.PluralRules) {
+    await import('@formatjs/intl-pluralrules/polyfill');
+    await import('@formatjs/intl-pluralrules/locale-data/en');
+    await import('@formatjs/intl-pluralrules/locale-data/pl');
+  }
+  if (!Intl.RelativeTimeFormat) {
+    await import('@formatjs/intl-relativetimeformat/polyfill');
+    await import('@formatjs/intl-relativetimeformat/locale-data/en');
+    await import('@formatjs/intl-relativetimeformat/locale-data/pl');
+  }
+};
+
+export default { en, pl };
