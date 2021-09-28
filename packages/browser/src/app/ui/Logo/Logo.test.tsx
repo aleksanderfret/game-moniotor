d6fcd217725a1c7c797100eaafa00566490097b9
Aleksander Fret <olekfret80@gmail.com>
2021-09-28 23:57:52 +0200

    [GR-8] Fix logo tests

--- packages/browser/src/app/ui/Logo/Logo.test.tsx
+++ packages/browser/src/app/ui/Logo/Logo.test.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
 
 import { render } from '__tests__/utils/render';
 import Logo from './Logo';
 
 describe('<Logo />', () => {
   test('should render', () => {
-    const { getByRole } = render(<Logo />);
+    render(<Logo />);
 
-    expect(getByRole('img', { name: 'Games Room' })).toBeVisible();
+    expect(screen.getByRole('img', { name: 'Games Room' })).toBeVisible();
   });
 
   test('should render in small size', () => {
-    const { getByTestId } = render(<Logo />);
+    render(<Logo />);
 
-    expect(getByTestId('logo')).toHaveStyle({
+    expect(screen.getByTestId('logo')).toHaveStyle({
       height: '3rem'
     });
   });
 
   test('should render in medium size', () => {
-    const { getByTestId } = render(<Logo size="medium" />);
+    render(<Logo size="medium" />);
 
-    expect(getByTestId('logo')).toHaveStyle({
+    expect(screen.getByTestId('logo')).toHaveStyle({
       height: '6rem'
     });
   });
 
   test('should render in big size', () => {
-    const { getByTestId } = render(<Logo size="big" />);
+    render(<Logo size="big" />);
 
-    expect(getByTestId('logo')).toHaveStyle({
+    expect(screen.getByTestId('logo')).toHaveStyle({
       height: '9rem'
     });
   });
 
   test('should render in large size', () => {
-    const { getByTestId } = render(<Logo size="large" />);
+    render(<Logo size="large" />);
 
-    expect(getByTestId('logo')).toHaveStyle({
+    expect(screen.getByTestId('logo')).toHaveStyle({
       height: '12rem'
     });
   });
