6240fb662f9d5e662853ea7510a017c65dc96736
Aleksander Fret <olekfret80@gmail.com>
2021-10-09 12:54:04 +0200

    [GR-10] Create button component

---
+++ packages/browser/src/ui/Button/Button.stories.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import Button, { ButtonProps } from './Button';
+
+export default {
+  title: 'UI/Button',
+  component: Button,
+  args: {
+    danger: false,
+    disabled: false,
+    label: 'Button',
+    loading: false,
+    onClick: () => alert('button clicked'),
+    primary: true,
+    secondary: false,
+    size: 'medium',
+    title: '',
+    type: 'button'
+  }
+} as Meta<typeof Button>;
+
+const Template: Story<ButtonProps> = args => <Button {...args}>Button</Button>;
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  size: 'small'
+};
+
+export const PrimarySmallDisabled = Template.bind({});
+PrimarySmallDisabled.args = {
+  disabled: true,
+  size: 'small'
+};
+
+export const PrimaryMedium = Template.bind({});
+PrimaryMedium.args = {
+  size: 'medium'
+};
+
+export const PrimaryMediumDisabled = Template.bind({});
+PrimaryMediumDisabled.args = {
+  disabled: true,
+  size: 'medium'
+};
+
+export const PrimaryBig = Template.bind({});
+PrimaryBig.args = {
+  size: 'big'
+};
+
+export const PrimaryBigDisabled = Template.bind({});
+PrimaryBigDisabled.args = {
+  disabled: true,
+  size: 'big'
+};
+
+export const SecondarySmall = Template.bind({});
+SecondarySmall.args = {
+  primary: false,
+  secondary: true,
+  size: 'small'
+};
+
+export const SecondarySmallDisabled = Template.bind({});
+SecondarySmallDisabled.args = {
+  disabled: true,
+  primary: false,
+  secondary: true,
+  size: 'small'
+};
+
+export const SecondaryMedium = Template.bind({});
+SecondaryMedium.args = {
+  primary: false,
+  secondary: true,
+  size: 'medium'
+};
+
+export const SecondaryMediumDisabled = Template.bind({});
+SecondaryMediumDisabled.args = {
+  disabled: true,
+  primary: false,
+  secondary: true,
+  size: 'medium'
+};
+
+export const SecondaryBig = Template.bind({});
+SecondaryBig.args = {
+  primary: false,
+  secondary: true,
+  size: 'big'
+};
+
+export const SecondaryBigDisabled = Template.bind({});
+SecondaryBigDisabled.args = {
+  disabled: true,
+  primary: false,
+  secondary: true,
+  size: 'big'
+};
--- packages/browser/src/ui/Button/Button.tsx
+++ packages/browser/src/ui/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentPropsWithoutRef, forwardRef } from 'react';
 import styled from 'styled-components';
 
+type ButtonSize = 'small' | 'medium' | 'big';
 export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   danger?: boolean;
   disabled?: boolean;
@@ -8,43 +9,118 @@ export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   loading?: boolean;
   primary?: boolean;
   secondary?: boolean;
+  size?: ButtonSize;
   title?: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
-const StyledButton = styled.button<ButtonProps>`
+interface StyledButtonProps extends Omit<ButtonProps, 'size'> {
+  size: number;
+}
+
+const PrimaryButton = styled.button<StyledButtonProps>`
   position: relative;
   font-family: inherit;
   letter-spacing: inherit;
   font-size: ${({ theme }) => theme.font.size.normal};
-  background: ${({ theme }) => theme.palette.primary.main};
+  font-weight: ${({ theme }) => theme.font.weight.regular};
+  background-color: ${({ disabled, theme }) =>
+    disabled ? theme.palette.gray[400] : theme.palette.primary.main};
   color: ${({ theme }) => theme.palette.primary.contrast};
   box-sizing: border-box;
   outline: 0;
-  border: 0;
+  border: 1px solid
+    ${({ disabled, theme }) =>
+      disabled ? theme.palette.gray[400] : theme.palette.primary.main};
   white-space: nowrap;
-  height: ${({ theme }) => theme.size.calc(3)};
-  max-height: ${({ theme }) => theme.size.calc(3)};
+  height: ${({ size, theme }) => theme.size.calc(size)};
+  max-height: ${({ size, theme }) => theme.size.calc(size)};
   border-radius: ${({ theme }) => theme.shape.borderRadius};
   display: inline-flex;
   align-items: center;
   justify-content: center;
   overflow: hidden;
-  padding: 0;
-  cursor: pointer;
+  padding: 0 ${({ theme }) => theme.size.calc(1)};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  box-shadow: ${({ theme }) => theme.shadows.init};
+  ${({ theme }) =>
+    theme.transition(
+      'color',
+      'background-color',
+      'border-color',
+      'box-shadow',
+      'transform'
+    )};
 
-  &:hover {
-    background: ${({ theme }) => theme.palette.primary.light};
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) => theme.palette.primary.dark};
+    border-color: ${({ theme }) => theme.palette.primary.dark};
   }
 
-  &:active {
-    background: ${({ theme }) => theme.palette.primary.dark};
+  &:active:not(:disabled) {
+    background-color: ${({ theme }) => theme.palette.primary.light};
+    border-color: ${({ theme }) => theme.palette.primary.light};
   }
 
-  &:focus {
+  &:focus:not(:disabled) {
     box-shadow: ${({ theme }) => theme.shadows.focus};
   }
 `;
+
+const DangerButton = styled(PrimaryButton)`
+  background-color: ${({ disabled, theme }) =>
+    disabled ? theme.palette.gray[400] : theme.palette.danger.dark};
+  border: 1px solid
+    ${({ disabled, theme }) =>
+      disabled ? theme.palette.gray[400] : theme.palette.danger.main};
+
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) => theme.palette.danger.dark};
+    border-color: ${({ theme }) => theme.palette.danger.dark};
+  }
+
+  &:active:not(:disabled) {
+    background-color: ${({ theme }) => theme.palette.danger.light};
+    border-color: ${({ theme }) => theme.palette.danger.light};
+  }
+
+  &:focus:not(:disabled) {
+    box-shadow: ${({ theme }) => theme.shadows.focusDanger};
+  }
+`;
+
+const SecondaryButton = styled(PrimaryButton)`
+  background-color: ${({ theme }) => theme.palette.background.light};
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.palette.gray[400] : theme.palette.primary.main};
+
+  &:hover:not(:disabled) {
+    background-color: ${({ theme }) =>
+      theme.tint(theme.palette.primary.dark, 85)};
+    border-color: ${({ theme }) => theme.palette.primary.dark};
+    color: ${({ theme }) => theme.palette.primary.dark};
+  }
+
+  &:active:not(:disabled) {
+    background-color: ${({ theme }) =>
+      theme.tint(theme.palette.primary.dark, 80)};
+    border-color: ${({ theme }) => theme.palette.primary.dark};
+    color: ${({ theme }) => theme.palette.primary.dark};
+  }
+`;
+
+const setSizeFactor = (size?: ButtonSize): number => {
+  switch (size) {
+    case 'small':
+      return 2;
+    case 'medium':
+      return 2.5;
+    case 'big':
+      return 3;
+    default:
+      return 2.5;
+  }
+};
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
@@ -57,6 +133,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       onClick,
       primary: sourcePrimary,
       secondary: sourceSecondary,
+      size,
       title,
       type = 'button',
       ...rest
@@ -66,6 +143,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     const disabled = sourceDisabled || loading;
     const secondary = sourceSecondary && !sourcePrimary;
     const primary = sourcePrimary || !secondary;
+
+    const Component = primary
+      ? danger
+        ? DangerButton
+        : PrimaryButton
+      : SecondaryButton;
 
     const handleClick = (
       event: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -78,7 +161,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     };
 
     return (
-      <StyledButton
+      <Component
         {...rest}
         aria-label={label}
         danger={danger}
@@ -87,11 +170,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         primary={primary}
         ref={ref}
         secondary={secondary}
+        size={setSizeFactor(size)}
         title={title}
         type={type}
       >
         {children}
-      </StyledButton>
+      </Component>
     );
   }
 );
