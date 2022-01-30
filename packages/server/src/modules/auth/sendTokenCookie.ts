5332c09f93883877cb257bbdff9f5c60ade8dac4
Aleksander Fret <olekfret80@gmail.com>
2022-01-30 18:20:45 +0100

    [GR-22] Increase cookie security settings on prodution

--- packages/server/src/modules/auth/sendTokenCookie.ts
+++ packages/server/src/modules/auth/sendTokenCookie.ts
@@ -1,11 +1,23 @@
 import { CookieOptions, Response } from 'express';
 
+import environment from 'env/environment';
+
+const { IS_PROD } = environment;
+
+const productionOptions: CookieOptions = IS_PROD
+  ? {
+      sameSite: true,
+      secure: true,
+    }
+  : {};
+
 export const sendTokenCookie =
   (name: string, path?: string) =>
   (res: Response, token: string, options: CookieOptions = {}) =>
     res.cookie(name, token, {
       httpOnly: true,
       path,
+      ...productionOptions,
       ...options,
     });
 
