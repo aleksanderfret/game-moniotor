1850521d08ba0bbc6b0117de190945065aae08bd
Aleksander Fret <olekfret80@gmail.com>
2022-03-06 16:26:45 +0100

    [GR-24] Create feedback component

---
+++ packages/browser/src/ui/Feedback/Feedback.tsx
@@ -0,0 +1,20 @@
+import React, { FC, ReactNode } from 'react';
+import Alert, { AlertProps } from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
+interface FeedbackProps extends AlertProps {
+  header?: string | ReactNode;
+  message: string | ReactNode;
+}
+
+const Feedback: FC<FeedbackProps> = ({
+  header,
+  message,
+  severity = 'error',
+}) => (
+  <Alert security={severity}>
+    {header && <AlertTitle>{header}</AlertTitle>}
+    {message}
+  </Alert>
+);
+export default Feedback;
