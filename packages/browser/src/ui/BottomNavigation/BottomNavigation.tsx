ac62212fa2cbe2ae999dfd08bbde47568b167513
Aleksander Fret <olekfret80@gmail.com>
2022-03-12 17:22:13 +0100

    [GR-24] Update Bottom Navigation

--- packages/browser/src/ui/BottomNavigation/BottomNavigation.tsx
+++ packages/browser/src/ui/BottomNavigation/BottomNavigation.tsx
@@ -1,37 +1,90 @@
 import React, { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { styled } from '@mui/material/styles';
 import { NavLink } from 'react-router-dom';
+import MuiBottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
+import {
+  EventsIcon,
+  GamesIcon,
+  HomeIcon,
+  PlaysIcon,
+  ToolsIcon,
+} from 'ui/Icons';
 import { Path } from 'router';
 
-const Link = styled(NavLink)(() => ({
-  flexGrow: 1,
-  justifyContent: 'center',
-  display: 'flex',
-  alignItems: 'center',
-  padding: '8px',
-  boxSizing: 'border-box',
+const Link = styled(NavLink)(({ theme }) => ({
+  '&.MuiButtonBase-root': {
+    color: theme.palette.primary.main,
+
+    '&.active': {
+      fontWeight: 'bold',
+
+      '& .MuiSvgIcon-root path': {
+        strokeWidth: '1.2',
+      },
+    },
+
+    '& .MuiBottomNavigationAction-label': {
+      [theme.breakpoints.down('sm')]: {
+        fontSize: theme.font.size.tiny,
+      },
+    },
+  },
 }));
 
-export const Box = styled('div')(() => ({
+const StyledBottomNavigation = styled(MuiBottomNavigation)(({ theme }) => ({
   display: 'flex',
   position: 'fixed',
-  left: 0,
-  right: 0,
   bottom: 0,
-  height: '64px',
+  width: '100%',
+  backgroundColor: theme.palette.background.paper,
+
+  [theme.breakpoints.up('md')]: {
+    display: 'none',
+  },
+
+  '& > :last-child': {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
 }));
 
-const BottomNavigation: FC = () => {
-  return (
-    <Box>
-      <Link to={Path.Dashboard}>Home</Link>
-      <Link to={Path.Games}>Games</Link>
-      <Link to={Path.Plays}>Plays</Link>
-      <Link to={Path.Events}>Events</Link>
-      <Link to={Path.Tools}>Tools</Link>
-    </Box>
-  );
-};
+const BottomNavigation: FC = () => (
+  <StyledBottomNavigation showLabels>
+    <BottomNavigationAction
+      component={Link}
+      icon={<HomeIcon />}
+      label={<FormattedMessage id="navigation.home" />}
+      to={Path.Dashboard}
+    />
+    <BottomNavigationAction
+      component={Link}
+      icon={<GamesIcon />}
+      label={<FormattedMessage id="navigation.games" />}
+      to={Path.Games}
+    />
+    <BottomNavigationAction
+      component={Link}
+      icon={<PlaysIcon />}
+      label={<FormattedMessage id="navigation.plays" />}
+      to={Path.Plays}
+    />
+    <BottomNavigationAction
+      component={Link}
+      icon={<EventsIcon />}
+      label={<FormattedMessage id="navigation.events" />}
+      to={Path.Events}
+    />
+    <BottomNavigationAction
+      component={Link}
+      icon={<ToolsIcon />}
+      label={<FormattedMessage id="navigation.tools" />}
+      to={Path.Tools}
+    />
+  </StyledBottomNavigation>
+);
 
 export default BottomNavigation;
